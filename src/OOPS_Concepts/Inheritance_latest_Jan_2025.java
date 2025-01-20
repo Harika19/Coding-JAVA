0232e47a6eae06249acfaaaff49e935ba3c8a63a
Harika19 <36102187+Harika19@users.noreply.github.com>
2025-01-21 00:27:53 +0530

    Create Inheritance_latest_Jan_2025.java

---
+++ src/OOPS_Concepts/Inheritance_latest_Jan_2025.java
@@ -0,0 +1,46 @@
+public class Main
+{
+
+	static class Base {
+		Base() {
+			System.out.println(
+			    "Base Class Constructor Called ");
+		}
+
+		public void hello() {
+			System.out.println("Hello Base");
+		}
+	}
+
+	static class Derived extends Base {
+		Derived() {
+			super();
+			System.out.println(
+			    "Derived Class Constructor Called ");
+		}
+
+		@Override
+		public void hello() {
+			System.out.println("Hello Derived");
+		}
+	}
+
+
+	public static void main(String[] args) {
+
+		System.out.println("\n-- Derived calling Derived class --\n");
+
+		Derived derived = new Derived();
+		derived.hello();
+
+		System.out.println("\n-- Base calling Derived class --\n");
+
+		Base baseDerived = new Derived();
+		baseDerived.hello();
+
+		System.out.println("\n-- Base calling Base class --\n");
+
+		Base base = new Base();
+		base.hello();
+	}
+}
